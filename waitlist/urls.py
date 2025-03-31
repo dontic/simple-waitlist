aea86e78b691ef67368ef98a96cc93338eac5852
Daniel Garcia <github@mail.daniel.es>
2025-03-31 11:31:53 +0000

    Update URL routing in waitlist app to register WaitlistUserViewSet under "users" instead of "waitlist-users".

--- waitlist/urls.py
+++ waitlist/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import WaitlistUserViewSet
 
 router = DefaultRouter()
-router.register("waitlist-users", WaitlistUserViewSet)
+router.register("users", WaitlistUserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
