bcbd37a8ae0b35ddea097b1aebd977c107673caf
Daniel Garcia <github@mail.daniel.es>
2025-03-31 09:18:06 +0000

    Update URL configuration to include media serving and API schema endpoints for Swagger UI in debug mode.

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + [
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/schema/swagger-ui/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+    ]
+    if settings.DEBUG
+    else []
+)
