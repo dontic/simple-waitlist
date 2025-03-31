e548b2377a52b0d848c71b9e3d39373a02ffc41a
Daniel Garcia <github@mail.daniel.es>
2025-03-31 11:09:25 +0000

    Update WaitlistAdmin and WaitlistItemAdmin to include created_at field in list display

--- waitlist/admin.py
+++ waitlist/admin.py
@@ -4,12 +4,12 @@ from .models import Waitlist, WaitlistItem
 
 @admin.register(Waitlist)
 class WaitlistAdmin(admin.ModelAdmin):
-    list_display = ("name", "ntfy_topic")
+    list_display = ("name", "ntfy_topic", "created_at")
     search_fields = ("name",)
 
 
 @admin.register(WaitlistItem)
 class WaitlistItemAdmin(admin.ModelAdmin):
-    list_display = ("email", "waitlist")
+    list_display = ("email", "waitlist", "created_at")
     list_filter = ("waitlist",)
     search_fields = ("email",)
